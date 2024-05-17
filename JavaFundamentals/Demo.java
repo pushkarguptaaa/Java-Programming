95a1810eb9b2505c3823dcaf67cdec28aeea2008
Pushkar Gupta <pushkardps8@gmail.com>
2024-05-17 23:49:05 +0530

    data types and methods

--- JavaFundamentals/Demo.java
+++ JavaFundamentals/Demo.java
@@ -2,6 +2,16 @@ package JavaFundamentals;
 class Demo
 {
     public static void main(String args[]) {
-        System.out.println("Hello World");
+        int a=10;
+        double d=4.6;
+        // float f=7.7f;
+        // char c='A';
+        System.out.println(a+" "+d);
+
+        show();
+    }
+
+    public static void show(){
+        System.out.println("in show");
     }
 }
